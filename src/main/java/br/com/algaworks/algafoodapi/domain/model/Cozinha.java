c873e070c04f23c74f71f5e45f731750df4bbfbd
Robson <robmartinhao@gmail.com>
2021-12-18 15:21:17 -0300

    3.15. Conhecendo e usando o Lombok

--- src/main/java/br/com/algaworks/algafoodapi/domain/model/Cozinha.java
+++ src/main/java/br/com/algaworks/algafoodapi/domain/model/Cozinha.java
@@ -1,44 +1,20 @@
 package br.com.algaworks.algafoodapi.domain.model;
 
-import javax.persistence.*;
-import java.util.Objects;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 30)
     private String nome;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return Objects.equals(id, cozinha.id) && Objects.equals(nome, cozinha.nome);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome);
-    }
 }

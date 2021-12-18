93b2864cc1761317e4dc32c1bbc7d6d5a81ad74d
Robson <robmartinhao@gmail.com>
2021-12-18 12:06:57 -0300

    3.8. Consultando objetos do banco de dados

---
+++ src/main/java/br/com/algaworks/algafoodapi/jpa/CadastroCozinha.java
@@ -0,0 +1,19 @@
+package br.com.algaworks.algafoodapi.jpa;
+
+import br.com.algaworks.algafoodapi.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    public List<Cozinha> listar() {
+        return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+    }
+}
---
+++ src/main/java/br/com/algaworks/algafoodapi/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,22 @@
+package br.com.algaworks.algafoodapi.jpa;
+
+import br.com.algaworks.algafoodapi.AlgafoodApiApplication;
+import br.com.algaworks.algafoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+        cozinhas.forEach(cozinha -> System.out.println(cozinha.getNome()));
+    }
+}

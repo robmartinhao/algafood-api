6caf955d76dcfd91dbb64ed78beb3f2c0dff1013
Robson <robmartinhao@gmail.com>
2022-01-29 15:38:02 -0300

    8.15. Criando um exception handler global com ResponseEntityExceptionHandler

--- src/main/java/br/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -5,14 +5,14 @@ import br.com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaExcept
 import br.com.algaworks.algafoodapi.domain.exception.NegocioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
@@ -47,17 +47,5 @@ public class ApiExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(problema);
-    }
-
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem("O Tipo de mídia não é aceito")
-                .build();
-
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                .body(problema);
-
     }
 }

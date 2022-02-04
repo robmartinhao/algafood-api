0ddf345a1c67b09c8eed255038ff9f73e9021b9d
Robson <robmartinhao@gmail.com>
2022-02-04 20:27:34 -0300

    9.11. Customizando e resolvendo mensagens de validação globais em Resource Bundle

--- src/main/java/br/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -10,6 +10,9 @@ import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,14 +28,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-
     public static final String MSG_ERRO_GENERICA_USUARIO_FINAL = "Ocorreu um erro interno inesperado no sistema. "
             + "Tente novamente e se o problema persistir, entre em contato com o administrador do sistema.";
+
+    @Autowired
+    private MessageSource messageSouce;
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -41,10 +47,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         BindingResult bindingResult = ex.getBindingResult();
         List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
-                .map(fieldError -> Problem.Field.builder()
+                .map(fieldError -> {
+                    String message = messageSouce.getMessage(fieldError, LocaleContextHolder.getLocale());
+                    return Problem.Field.builder()
                         .name(fieldError.getField())
-                        .userMessage(fieldError.getDefaultMessage())
-                        .build()).collect(Collectors.toList());
+                        .userMessage(message)
+                        .build();
+                }).collect(Collectors.toList());
 
         Problem problem = createProblemBuilder(status, problemType, detail)
                 .userMessage(detail)

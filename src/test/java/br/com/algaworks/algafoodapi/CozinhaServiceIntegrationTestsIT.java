23bbbc75a6ab0e14dea05cab2867ee7a0c3ae7ed
Robson <robmartinhao@gmail.com>
2022-02-07 18:51:12 -0300

    10.7. Configurando Maven Failsafe Plugin no projeto -  ./mvnw verify

--- src/test/java/br/com/algaworks/algafoodapi/CozinhaServiceIntegrationTestsIT.java
+++ src/test/java/br/com/algaworks/algafoodapi/CozinhaServiceIntegrationTestsIT.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class CozinhaServiceIntegrationTests {
+public class CozinhaServiceIntegrationTestsIT {
 
     @Autowired
     CozinhaService cozinhaService;

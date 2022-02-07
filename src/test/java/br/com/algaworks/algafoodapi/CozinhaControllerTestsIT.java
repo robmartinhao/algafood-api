0ce3d8fc3906b33ea9f6398165d3ab6f44de2a69
Robson <robmartinhao@gmail.com>
2022-02-07 20:22:54 -0300

    10.9. Validando o corpo da resposta HTTP

--- src/test/java/br/com/algaworks/algafoodapi/CozinhaControllerTestsIT.java
+++ src/test/java/br/com/algaworks/algafoodapi/CozinhaControllerTestsIT.java
@@ -2,6 +2,7 @@ package br.com.algaworks.algafoodapi;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 
 @ExtendWith(SpringExtension.class)
@@ -23,12 +25,26 @@ public class CozinhaControllerTestsIT {
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
         enableLoggingOfRequestAndResponseIfValidationFails();
         given()
-                    .basePath("/cozinhas")
-                    .port(port)
-                    .accept(ContentType.JSON)
-                .when()
-                    .get()
-                .then()
-                    .statusCode(HttpStatus.OK.value());
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+        enableLoggingOfRequestAndResponseIfValidationFails();
+        given()
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .body("", hasSize(4))
+            .body("nome", hasItems("Indiana", "Tailandesa"));
     }
 }

cf6c3ae079c4e0b91d941271aa36b1e422e778df
Robson <robmartinhao@gmail.com>
2022-03-03 20:58:00 -0300

    13.7. Tratando BindException ao enviar parâmetros de URL inválidos

--- src/main/java/br/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -40,6 +41,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSouce;
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        return handleValidationInternal(ex, ex.getBindingResult(), status, headers, request);
+    }
 
     @ExceptionHandler(ValidacaoException.class)
     public ResponseEntity<Object> handleValidacao(ValidacaoException ex, WebRequest request) {

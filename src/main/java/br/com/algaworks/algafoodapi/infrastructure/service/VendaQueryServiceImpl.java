6d37804a30a6274c2b02c68e41dcbdb5761a6d21
Robson <robmartinhao@gmail.com>
2022-03-17 20:49:02 -0300

    13.15. Desafio: adicionando os filtros na consulta de vendas diárias

--- src/main/java/br/com/algaworks/algafoodapi/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/br/com/algaworks/algafoodapi/infrastructure/service/VendaQueryServiceImpl.java
@@ -2,12 +2,16 @@ package br.com.algaworks.algafoodapi.infrastructure.service;
 
 import br.com.algaworks.algafoodapi.domain.filter.VendaDiariaFilter;
 import br.com.algaworks.algafoodapi.domain.model.Pedido;
+import br.com.algaworks.algafoodapi.domain.model.StatusPedido;
 import br.com.algaworks.algafoodapi.domain.model.dto.VendaDiaria;
 import br.com.algaworks.algafoodapi.domain.service.VendaQueryService;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -30,6 +34,20 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 builder.count(root.get("id")),
                 builder.sum(root.get("valorTotal"))
         );
+
+        var predicates = new ArrayList<Predicate>();
+        if (filtro.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+        }
+        if (filtro.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+        }
+        if (filtro.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+        }
+        predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+        query.where(predicates.toArray(new Predicate[0]));
+
         query.select(selection);
         query.groupBy(functionDateDataCriacao);
 

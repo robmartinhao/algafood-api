26b123da1ec0aa9eac1410b78abcf336ac745ce3
Robson <robmartinhao@gmail.com>
2022-03-19 11:12:54 -0300

    14.3. Validando o tamanho máximo do arquivo

---
+++ src/main/java/br/com/algaworks/algafoodapi/core/validation/FileSizeValidator.java
@@ -0,0 +1,22 @@
+package br.com.algaworks.algafoodapi.core.validation;
+
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class FileSizeValidator implements ConstraintValidator<FileSize, MultipartFile> {
+
+    private DataSize maxSize;
+
+    @Override
+    public void initialize(FileSize constraintAnnotation) {
+        this.maxSize = DataSize.parse(constraintAnnotation.max());
+    }
+
+    @Override
+    public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext constraintValidatorContext) {
+        return multipartFile == null || multipartFile.getSize() <= maxSize.toBytes();
+    }
+}

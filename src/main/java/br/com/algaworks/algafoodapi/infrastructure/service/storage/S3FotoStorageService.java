7703b1c75f03e230ec5933b45579224239edcbef
Robson <robmartinhao@gmail.com>
2022-03-26 14:08:19 -0300

    14.21. Adicionando o SDK Java da Amazon S3 no projeto e criando classe do serviço de storage

---
+++ src/main/java/br/com/algaworks/algafoodapi/infrastructure/service/storage/S3FotoStorageService.java
@@ -0,0 +1,25 @@
+package br.com.algaworks.algafoodapi.infrastructure.service.storage;
+
+import br.com.algaworks.algafoodapi.domain.service.FotoStorageService;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+
+@Service
+public class S3FotoStorageService implements FotoStorageService {
+
+    @Override
+    public InputStream recuperar(String nomeArquivo) {
+        return null;
+    }
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+
+    }
+
+    @Override
+    public void remover(String nomeArquivo) {
+
+    }
+}

31eb38690109ecec3287637c6b26b7260c61b59c
Robson <robmartinhao@gmail.com>
2022-03-29 20:52:47 -0300

    15.3. Implementando o serviço de infraestrutura de envio de e-mails com Spring

---
+++ src/main/java/br/com/algaworks/algafoodapi/domain/service/EnvioEmailService.java
@@ -0,0 +1,19 @@
+package br.com.algaworks.algafoodapi.domain.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+public interface EnvioEmailService {
+
+    void enviar(Mensagem mensagem);
+
+    @Getter
+    @Setter
+    class Mensagem {
+        private Set<String> destinatarios;
+        private String assunto;
+        private String corpo;
+    }
+}
---
+++ src/main/java/br/com/algaworks/algafoodapi/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -0,0 +1,37 @@
+package br.com.algaworks.algafoodapi.infrastructure.service.email;
+
+import br.com.algaworks.algafoodapi.core.email.EmailPropeties;
+import br.com.algaworks.algafoodapi.domain.service.EnvioEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class SmtpEnvioEmailService implements EnvioEmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private EmailPropeties emailPropeties;
+
+    @Override
+    public void enviar(Mensagem mensagem) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+            helper.setFrom(emailPropeties.getRemetente());
+            helper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
+            helper.setSubject(mensagem.getAssunto());
+            helper.setText(mensagem.getCorpo(), true);
+
+            mailSender.send(mimeMessage);
+        } catch (Exception e) {
+            throw new EmailException("Não foi possível enviar e-mail", e);
+        }
+    }
+}

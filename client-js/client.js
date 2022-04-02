7e37860c3be2fd42f1f028c6284ee8f5ed976d94
Robson <robmartinhao@gmail.com>
2022-04-02 13:18:41 -0300

    16.1. Implementando uma chamada na REST API com JavaScript

---
+++ client-js/client.js
@@ -0,0 +1,11 @@
+function consultarRestaurantes() {
+    $.ajax({
+        url: "http://localhost:8080/restaurantes",
+        type: "get",
+        success: function(response) {
+            $("#conteudo").text(response);
+        }
+    });
+}
+
+$("#botao").click(consultarRestaurantes);

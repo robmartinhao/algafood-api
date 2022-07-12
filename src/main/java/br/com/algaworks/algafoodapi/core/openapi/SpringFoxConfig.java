3fbba5f49339d1265c540eef7f83027850edf392
Robson Martinhão <robmartinhao@gmail.com>
2022-07-12 19:29:24 -0300

    18.6. Descrevendo informações da API na documentação

--- src/main/java/br/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package br.com.algaworks.algafoodapi.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -14,9 +17,20 @@ public class SpringFoxConfig {
    public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.algaworks.algafoodapi"))
-                .paths(PathSelectors.any())
+                    .apis(RequestHandlerSelectors.basePackage("br.com.algaworks.algafoodapi"))
+                    .paths(PathSelectors.any())
                 //          .paths(PathSelectors.ant("/restaurantes/*"))
-                .build();
+                    .build()
+                .apiInfo(apiInfo());
    }
+
+   public ApiInfo apiInfo() {
+       return new ApiInfoBuilder()
+               .title("AlgaFood API")
+               .description("API aberta para clientes e restaurantes")
+               .version("1")
+               .contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
+               .build();
+   }
+
 }

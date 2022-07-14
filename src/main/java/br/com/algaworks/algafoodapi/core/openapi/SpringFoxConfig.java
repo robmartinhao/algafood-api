5ea24c4b5b6e92594815f66c4f1ef074df83998b
Robson Martinhão <robmartinhao@gmail.com>
2022-07-14 18:18:41 -0300

    18.12. Descrevendo códigos de status de respostas de forma global

--- src/main/java/br/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
@@ -3,38 +3,60 @@ package br.com.algaworks.algafoodapi.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
 
-   @Bean
-   public Docket apiDocket() {
+    @Bean
+    public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                    .apis(RequestHandlerSelectors.basePackage("br.com.algaworks.algafoodapi"))
-                    .paths(PathSelectors.any())
-                    .build()
+                .apis(RequestHandlerSelectors.basePackage("br.com.algaworks.algafoodapi"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, globalGetResponseMessages())
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"));
-   }
+    }
 
-   public ApiInfo apiInfo() {
-       return new ApiInfoBuilder()
-               .title("AlgaFood API")
-               .description("API aberta para clientes e restaurantes")
-               .version("1")
-               .contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
-               .build();
-   }
+    private List<Response> globalGetResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do Servidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                        .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+                        .build()
+        );
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("AlgaFood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("1")
+                .contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
+                .build();
+    }
 
 }

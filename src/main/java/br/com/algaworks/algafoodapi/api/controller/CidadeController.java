4f45d46737af36c8ac2244c3648dd5e351ad05d4
Robson Martinhão <robmartinhao@gmail.com>
2022-07-15 18:48:05 -0300

    18.16. Descrevendo códigos de status de respostas em endpoints específicos

--- src/main/java/br/com/algaworks/algafoodapi/api/controller/CidadeController.java
+++ src/main/java/br/com/algaworks/algafoodapi/api/controller/CidadeController.java
@@ -2,6 +2,7 @@ package br.com.algaworks.algafoodapi.api.controller;
 
 import br.com.algaworks.algafoodapi.api.converter.domain.CidadeDomainConverter;
 import br.com.algaworks.algafoodapi.api.converter.output.CidadeOutputConverter;
+import br.com.algaworks.algafoodapi.api.exceptionhandler.Problem;
 import br.com.algaworks.algafoodapi.api.model.dto.input.CidadeInput;
 import br.com.algaworks.algafoodapi.api.model.dto.output.CidadeOutput;
 import br.com.algaworks.algafoodapi.domain.exception.EstadoNaoEncontradoException;
@@ -9,15 +10,14 @@ import br.com.algaworks.algafoodapi.domain.exception.NegocioException;
 import br.com.algaworks.algafoodapi.domain.model.Cidade;
 import br.com.algaworks.algafoodapi.domain.repository.CidadeRepository;
 import br.com.algaworks.algafoodapi.domain.service.CidadeService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+
 @Api(tags = "Cidades")
 @RestController
 @RequestMapping("/cidades")
@@ -42,16 +42,23 @@ public class CidadeController {
     }
 
     @ApiOperation("Busca uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "ID da cidade inválido", response = Problem.class),
+            @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+    })
     @GetMapping("/{id}")
     public CidadeOutput buscarPeloId(@ApiParam(value = "ID de uma cidade") @PathVariable Long id) {
         return cidadeOutputConverter.toCidadeOutput(cidadeService.buscarOuFalhar(id));
     }
 
     @ApiOperation("Cadastra uma cidade")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cidade cadastrada")
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CidadeOutput salvar(@ApiParam(value = "Representação de uma nova cidade")
-                                   @RequestBody @Valid CidadeInput cidadeInput) {
+                               @RequestBody @Valid CidadeInput cidadeInput) {
         try {
             Cidade cidade = cidadeDomainConverter.toDomainObject(cidadeInput);
             return cidadeOutputConverter.toCidadeOutput(cidadeService.salvar(cidade));
@@ -61,6 +68,10 @@ public class CidadeController {
     }
 
     @ApiOperation("Atualiza uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(code= 200, message = "Cidade atualizada"),
+            @ApiResponse(code= 404, message = "Cidade não encontrada", response = Problem.class)
+    })
     @PutMapping("/{id}")
     public CidadeOutput atualizar(@ApiParam(value = "ID de uma cidade")
                                   @PathVariable Long id,
@@ -78,6 +89,10 @@ public class CidadeController {
     }
 
     @ApiOperation("Exclui uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(code= 204, message = "Cidade excluída"),
+            @ApiResponse(code= 404, message = "Cidade não encontrada", response = Problem.class)
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(

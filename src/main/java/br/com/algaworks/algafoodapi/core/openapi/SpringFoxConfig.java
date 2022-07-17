49d6496b68a9c4206796b0500a9e494943d0ddcc
Robson Martinhão <robmartinhao@gmail.com>
2022-07-17 14:26:15 -0300

    18.25. Descrevendo parâmetros globais em operações

--- src/main/java/br/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
@@ -18,15 +18,14 @@ import org.springframework.web.context.request.ServletWebRequest;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.*;
 import springfox.documentation.schema.AlternateTypeRules;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.Response;
-import springfox.documentation.service.Tag;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -53,6 +52,15 @@ public class SpringFoxConfig {
                 .globalResponses(HttpMethod.POST, globalPosPuttResponseMessages())
                 .globalResponses(HttpMethod.PUT, globalPosPuttResponseMessages())
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+                .globalRequestParameters(Collections.singletonList(
+                        new RequestParameterBuilder()
+                            .name("campos")
+                            .description("Nomes das propriedades para filtrar na resposta, separados por vírgula")
+                                .in(ParameterType.QUERY)
+                                .required(Boolean.TRUE)
+                                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                                .build()
+                ))
                 .additionalModels(TypeResolver.resolve(Problem.class))
                 .ignoredParameterTypes(ServletWebRequest.class)
                 .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)

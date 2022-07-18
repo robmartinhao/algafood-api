6df3ebaef0f9f57c5c5862c8e11dbc315085608f
Robson Martinhão <robmartinhao@gmail.com>
2022-07-18 18:33:27 -0300

    18.31. Desafio: descrevendo documentação de endpoints de fluxo de pedidos

--- src/main/java/br/com/algaworks/algafoodapi/api/controller/FluxoPedidoController.java
+++ src/main/java/br/com/algaworks/algafoodapi/api/controller/FluxoPedidoController.java
@@ -3,6 +3,7 @@ package br.com.algaworks.algafoodapi.api.controller;
 import br.com.algaworks.algafoodapi.domain.service.FluxoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,19 +13,19 @@ public class FluxoPedidoController {
     @Autowired
     private FluxoPedidoService fluxoPedidoService;
 
-    @PutMapping("/confirmacao")
+    @PutMapping(value = "/confirmacao", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void confirmar(@PathVariable String codigoPedido) {
         fluxoPedidoService.confirmar(codigoPedido);
     }
 
-    @PutMapping("/entrega")
+    @PutMapping(path ="/entrega", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void entregar(@PathVariable String codigoPedido) {
         fluxoPedidoService.entregar(codigoPedido);
     }
 
-    @PutMapping("/cancelamento")
+    @PutMapping(value = "/cancelamento", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void cancelar(@PathVariable String codigoPedido) {
         fluxoPedidoService.cancelar(codigoPedido);

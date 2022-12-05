9a949d39dbc306623c7091e550999bfbfd9c7269
Robson Martinhão <robmartinhao@gmail.com>
2022-12-05 20:28:46 -0300

    26.6. Descrevendo tags na documentação e associando com controllers

--- src/main/java/br/com/algaworks/algafoodapi/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/br/com/algaworks/algafoodapi/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -3,10 +3,12 @@ package br.com.algaworks.algafoodapi.api.v1.openapi.controller;
 import br.com.algaworks.algafoodapi.api.v1.model.dto.input.CidadeInput;
 import br.com.algaworks.algafoodapi.api.v1.model.dto.output.CidadeOutput;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Cidades")
 public interface CidadeControllerOpenApi {
     CollectionModel<CidadeOutput> listar();
 
--- src/main/java/br/com/algaworks/algafoodapi/core/springdoc/SpringDocConfig.java
+++ src/main/java/br/com/algaworks/algafoodapi/core/springdoc/SpringDocConfig.java
@@ -9,8 +9,11 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 @SecurityScheme(name = "security_auth",
@@ -40,7 +43,9 @@ public class SpringDocConfig {
                 ).externalDocs(new ExternalDocumentation()
                         .description("AlgaWoks")
                         .url("https://algaworks.com")
-                );
+                ).tags(Arrays.asList(
+                        new Tag().name("Cidades").description("Gerencia as cidades")
+                ));
     }
 
 //    @Bean

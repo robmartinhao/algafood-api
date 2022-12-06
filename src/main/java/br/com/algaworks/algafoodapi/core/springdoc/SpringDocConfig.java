0bede7634628c307164fec4029c8c18c13133c47
Robson Martinhão <robmartinhao@gmail.com>
2022-12-06 10:01:40 -0300

    26.11. Descrevendo códigos de status de respostas de forma global

--- src/main/java/br/com/algaworks/algafoodapi/core/springdoc/SpringDocConfig.java
+++ src/main/java/br/com/algaworks/algafoodapi/core/springdoc/SpringDocConfig.java
@@ -9,7 +9,10 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -46,6 +49,28 @@ public class SpringDocConfig {
                 ).tags(Arrays.asList(
                         new Tag().name("Cidades").description("Gerencia as cidades")
                 ));
+    }
+
+    @Bean
+    public OpenApiCustomiser openApiCustomiser() {
+        return  openApi -> {
+            openApi.getPaths()
+                    .values()
+                    .stream()
+                    .flatMap(pathItem -> pathItem.readOperations().stream())
+                    .forEach(operation -> {
+                        ApiResponses responses = operation.getResponses();
+
+                        ApiResponse apiResponseErroInterno = new ApiResponse().description("Erro interno no servidor");
+                        ApiResponse apiResponseNaoEncontrado = new ApiResponse().description("Recurso não encontrado");
+                        ApiResponse apiResponseSemRespresentacao = new ApiResponse().description("Recurso não possui uma" +
+                                "representacao que poderia ser aceita pelo consumidor");
+
+                        responses.addApiResponse("500", apiResponseErroInterno);
+                        responses.addApiResponse("404", apiResponseNaoEncontrado);
+                        responses.addApiResponse("406", apiResponseSemRespresentacao);
+                    });
+        };
     }
 
 //    @Bean

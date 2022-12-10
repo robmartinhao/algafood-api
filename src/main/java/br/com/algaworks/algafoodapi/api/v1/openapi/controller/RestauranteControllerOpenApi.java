646f2af738437360ded369d1d86c7eaa9035ddb7
Robson Martinhão <robmartinhao@gmail.com>
2022-12-10 11:47:36 -0300

    26.21. Descrevendo parâmetros de projeções em endpoints de consultas

--- src/main/java/br/com/algaworks/algafoodapi/api/v1/openapi/controller/RestauranteControllerOpenApi.java
+++ src/main/java/br/com/algaworks/algafoodapi/api/v1/openapi/controller/RestauranteControllerOpenApi.java
@@ -4,6 +4,9 @@ import br.com.algaworks.algafoodapi.api.v1.model.dto.input.RestauranteInput;
 import br.com.algaworks.algafoodapi.api.v1.model.dto.output.RestauranteApenasNomeOutput;
 import br.com.algaworks.algafoodapi.api.v1.model.dto.output.RestauranteBasicoOutput;
 import br.com.algaworks.algafoodapi.api.v1.model.dto.output.RestauranteOutput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +16,16 @@ import java.util.List;
 @SecurityRequirement(name = "security_auth")
 public interface RestauranteControllerOpenApi {
 
+    @Operation(parameters = {
+            @Parameter(name = "projecao",
+            description = "Nome da projeção",
+            example = "apenas-nome",
+            in = ParameterIn.QUERY,
+            required = true)
+    })
     CollectionModel<RestauranteBasicoOutput> listar();
 
+    @Operation(hidden = true)
     CollectionModel<RestauranteApenasNomeOutput> listarApenasNomes();
 
     RestauranteOutput buscarPeloId(Long restauranteId);

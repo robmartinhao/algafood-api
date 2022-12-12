06165814a548bd9d8f27e5beacdd3eaa4d9fbdd5
Robson Martinhão <robmartinhao@gmail.com>
2022-12-12 11:35:10 -0300

    26.35. Desafio: descrevendo documentação de endpoint de permissões

--- src/main/java/br/com/algaworks/algafoodapi/api/v1/openapi/controller/PermissaoControllerOpenApi.java
+++ src/main/java/br/com/algaworks/algafoodapi/api/v1/openapi/controller/PermissaoControllerOpenApi.java
@@ -1,10 +1,15 @@
 package br.com.algaworks.algafoodapi.api.v1.openapi.controller;
 
 import br.com.algaworks.algafoodapi.api.v1.model.dto.output.PermissaoOutput;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.hateoas.CollectionModel;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Permissões")
 public interface PermissaoControllerOpenApi {
+    @Operation(summary = "Lista as permissões")
     CollectionModel<PermissaoOutput> listar();
 }

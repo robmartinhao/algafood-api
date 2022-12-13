ed03cd8cdfe7611bf7afe58d4896037eb89eb6f6
Robson Martinhão <robmartinhao@gmail.com>
2022-12-13 15:46:20 -0300

    27.12. Testando o fluxo Authorization Code + PKCE + S256 e corrigindo problemas

--- src/main/java/br/com/algaworks/algafoodapi/core/security/ResourceServerConfig.java
+++ src/main/java/br/com/algaworks/algafoodapi/core/security/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package br.com.algaworks.algafoodapi.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +23,6 @@ public class ResourceServerConfig {
                 .and()
                 .oauth2ResourceServer().jwt();
 
-        return http.build();
+        return http.formLogin(Customizer.withDefaults()).build();
     }
 }
--- src/main/java/br/com/algaworks/algafoodapi/core/security/authorizationserver/JpaUserDetailService.java
+++ src/main/java/br/com/algaworks/algafoodapi/core/security/authorizationserver/JpaUserDetailService.java
@@ -5,6 +5,7 @@ import br.com.algaworks.algafoodapi.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -24,7 +25,7 @@ public class JpaUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Usuario usuario = usuarioRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o e-mail informado"));
-        return new AuthUser(usuario, getAuthorithies(usuario));
+        return new User(usuario.getEmail(), usuario.getSenha(), getAuthorithies(usuario));
     }
 
     private Collection<GrantedAuthority> getAuthorithies(Usuario usuario) {
